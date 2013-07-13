6cac24b75919026bd612bb5537d58f080a389f45
julien DEVILLE <julien.deville@gmail.com>
2013-07-12 20:06:49 -0400

    debut map 1

---
+++ Assets/cases arc dessus/Maps/god.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+//using UnityEditor;
+
+public class god : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//EditorApplication.NewScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
